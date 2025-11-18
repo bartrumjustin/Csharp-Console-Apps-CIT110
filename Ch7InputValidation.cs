56ae694729613ffb1a7bb3849d391035129dbc02
Justin Bartrum <141288505+bartrumjustin@users.noreply.github.com>
2025-11-17 21:23:06 -0500

    Chapter 7
    using type methods to validate inputs using various types

---
+++ Ch7InputValidation.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Xml.Linq;
+namespace FunwithLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> members = new List<string> { "Mickey", "Minnie", "Donald", "Daisy" };
+            string? name = String.Empty;
+            string[] nameArr;
+            int? menu = 0;
+            //Removed redundant call for input validation as while loop's else condition will invoke call
+            
+            
+            while (menu != 4)
+            {
+                if (menu == 1)
+                {
+                    printList(members);
+                    menu = 0;
+                }
+                else if (menu == 2)
+                {
+                    nameArr = addList();
+                    members.AddRange(nameArr);
+                    menu = 0;
+                }
+                else if (menu == 3)
+                {
+                   
+                    name = NameValidation();
+                    if (members.Contains(name))
+                    {
+                        members.Remove(name);
+                        Console.WriteLine("The name has been deleted");
+                        menu = 0;
+                    }
+                    else
+                        Console.WriteLine("Sorry, that name does not exist, please try again");
+                }
+                else
+                    menu = InputValidation();
+            }
+
+
+        }
+        static void printList(List<string> mem)
+        {
+            mem.Sort();
+            foreach (string s in mem)
+                Console.WriteLine(s);
+            return;
+        }
+        static string[] addList()
+        {
+            bool isIntFlag= false;
+            int number = 0;
+            while (isIntFlag==false)
+            {
+                Console.Write("How many members do you wish to add? ");
+
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    isIntFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid Integer");
+                }
+                    
+            }
+            
+            string[] newMembers = new string[number];
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("Member name? ");
+                
+                string tempName = Console.ReadLine();
+                while (string.IsNullOrEmpty(tempName)==true || string.IsNullOrWhiteSpace(tempName)==true){
+                    
+                    Console.WriteLine($"Your name entry, {tempName}, is invalid, please try again");
+                    tempName = Console.ReadLine();
+                }
+                newMembers[i] = tempName;
+                Console.WriteLine("Valid entry, Name has been added to the list");
+            }
+            return newMembers;
+        }
+        static int InputValidation() 
+        {
+            int parsedInt = 0;
+            bool isItNumber = false;
+            int selectmin = 1;
+            int selectmax = 4;
+            
+                Console.WriteLine("Please make a selection from the menu");
+                Console.WriteLine("1. Print sorted list\n2. Add to List\n3. Delete List\n4. Quit");
+                while(!int.TryParse(Console.ReadLine(), out parsedInt) || parsedInt<selectmin||parsedInt>selectmax)
+                {
+                    Console.WriteLine("Select a valid selection, please.");
+                }
+              
+            return parsedInt;
+        }
+        static string NameValidation()
+        {
+            string validName = string.Empty;
+            
+            bool valid = false;
+            while (valid == false)
+            {
+                Console.Write("Enter the name of the person you want removed from the list: ");
+                validName = Console.ReadLine();
+                bool emptyOrNull = string.IsNullOrEmpty(validName);
+                bool whiteSpace = string.IsNullOrWhiteSpace(validName);
+                if (emptyOrNull == true || whiteSpace == true)
+                {
+                    Console.WriteLine("The name provided is not a valid string, try again.");
+                }
+                else
+                {
+                    Console.WriteLine("The name is a valid string");
+                    valid = true;
+                }
+            }
+            return validName;
+        }
+    }
+}
---
+++ Ch7MathMethods.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Ch7MathMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            string test = "";
+            bool testFlag = false;
+            
+            
+                for (int i = 1; i < 3; i++) {
+                while (testFlag == false)
+                {
+                    Console.WriteLine($"Pick a number for n{i}");
+                    test = Console.ReadLine();
+                    testFlag = ValidateInt(test);
+                }
+                if (i == 1)
+                {
+                   n1=int.Parse(test);
+                    testFlag = false;
+                }
+                else
+                {
+                    n2=int.Parse(test);
+                }
+            }
+            int max = Math.Max(n1, n2);
+            Console.WriteLine($"Max is {max}");
+            int min = Math.Min(n1, n2);
+            Console.WriteLine($"Min is {min}");
+            double pown1 = Math.Pow(n1,2);
+            Console.WriteLine($"n1 to the power of 2 is {pown1}");
+            double pown2 = Math.Pow(n2,2);
+            Console.WriteLine($"n2 to the power of 2 is {pown2}");
+            double sqr1 = Math.Sqrt(n1);
+            Console.WriteLine($"square root of n1 {n1} is {sqr1}");
+            double sqr2 = Math.Sqrt(n2);
+            Console.WriteLine($"square root of n2 {n2} is {sqr2}");
+            if (Math.Abs(n2) > 0)
+            {
+                int div = n1 / n2;
+                int rem = n1 % n2;
+                Console.WriteLine($"dividing n2 by n1 is {div} with {rem} as the remainder");
+            }
+            else
+            {
+                Console.WriteLine("Could not perform a division and remainder method");
+            }
+
+        }
+        static bool ValidateInt(string test)
+        {
+            int examined;
+            bool result = false;
+            if (int.TryParse(test, out examined)) {
+              result = true;
+            } return result;
+        }
+    }
+}
